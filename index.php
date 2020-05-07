db6f304d337c6a19c41a86fccc08d466d73f1a12
Jani Haiko <haiko.jani@gmail.com>
2020-05-07 20:51:39 +0300

    maintenance update

--- index.php
+++ index.php
@@ -16,10 +16,11 @@
         <meta name="keywords" content="translatefyer,translate,translation,machine translation,Google Translate" />
         <meta name="description" content="Run texts throught multiple layers of machine translation." />
         <meta name="author" content="Jani Haiko, ojaha065@edu.xamk.fi" />
-        <meta name="revised" content="Thursday, July 4th, 2018" />
+        <meta name="revised" content="Wednesday, May 6th, 2020" />
         <meta name="rating" content="General" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" type="text/css" media="all" />
         <link rel="stylesheet" href="styles.css" type="text/css" media="all" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="languageCodes.js" async></script>
         <script src="scripts.js"></script>
     </head>
@@ -30,8 +31,8 @@
             <p><b>What is Translatefyer?</b> Translatefyer runs texts throught multiple layers of Google's machine translation. As you know, Google Translate often returns hilarious results. We just take it a step further by running the input throught Google Translator multiple times and switching languages between.</p>
             <p><b>But why?</b> I dunno, for fun, I guess? This project was heavily inspired by the popular <a href="https://www.youtube.com/watch?v=957K58gMbPA" target="_blank">Google Translate Sings</a> videos by the YouTube channel <a href="https://www.youtube.com/channel/UCP2-S6-M9ZvlY8t7cRn4O6A" target="_blank">Translator fails</a>.</p>
             <p><b>How does it work?</b> It's just using <a href="https://cloud.google.com/translate/" target="_blank">Google's translation API</a>. For more technical details, you can check it's <a href="https://github.com/ojaha065/translatefyer" target="_blank">GitHub page</a>.</p>
-            <p><b>Who is/are behind this? I want to contact you. How I can do that?</b> I'm just a IT-student from Finland. Feel free to to shoot me an email: haiko.jani [at] gmail [dot] com.</p>
-            <p id="noticeAboutLimits"><b>Update 4/2020:</b> Due to the increased pricing of Google Cloud Translation, I implemented a new limitation. Only up to 20 000 characters can be processed per day. <strong>This limit is shared between all users</strong>. If you get an error message, please try again after 24 hrs. I'm really sorry about this.</p>
+            <p><b>Who is/are behind this? I want to contact you. How I can do that?</b> I'm just a IT-student from Finland. Feel free to to shoot me an email (haiko.jani [at] gmail [dot] com) or contact me via <a href="https://t.me/JakeRaccoon" target="_blank"><span class="fab fa-telegram"></span>Telegram</a>.</p>
+            <p id="noticeAboutLimits"><b>Update 5/2020:</b> Due to the increased pricing of Google Cloud Translation, I implemented a new limitation. Only up to 50 000 characters can be processed per day. <strong>This limit is shared between all users</strong>. If you get an error message, please try again after 24 hrs. I'm really sorry about this.</p>
         </div>
         <textarea id="sourceText" maxlength="1200" placeholder="Type or paste here." required></textarea>
         <small id="errorArea"></small>
